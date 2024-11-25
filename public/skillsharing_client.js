de546567c65d77a95c12b7e51997c1f564b4ff07
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2024-11-25 16:29:28 +0200

    changed the code in skillsharing_client.js file to improve templates

--- public/skillsharing_client.js
+++ public/skillsharing_client.js
@@ -44,15 +44,15 @@ function reportError(error) {
   alert(String(error));
 }
 
-function renderUserField(name, dispatch) {
-  return elt("label", {}, "Your name: ", elt("input", {
-    type: "text",
-    value: name,
-    onchange(event) {
-      dispatch({type: "setUser", user: event.target.value});
-    }
-  }));
-}
+// function renderUserField(name, dispatch) {
+//   return elt("label", {}, "Your name: ", elt("input", {
+//     type: "text",
+//     value: name,
+//     onchange(event) {
+//       dispatch({type: "setUser", user: event.target.value});
+//     }
+//   }));
+// }
 
 function elt(type, props, ...children) {
   let dom = document.createElement(type);
@@ -64,72 +64,138 @@ function elt(type, props, ...children) {
   return dom;
 }
 
-// Функция для обновления поля комментария без потери содержимого и фокуса
-function updateCommentsUI(talkElement, comments) {
-  // Найти поле для ввода нового комментария
-  const commentInput = talkElement.querySelector(".new-comment");
+// // Функция для обновления поля комментария без потери содержимого и фокуса
+// function updateCommentsUI(talkElement, comments) {
+//   // Найти поле для ввода нового комментария
+//   const commentInput = talkElement.querySelector(".new-comment");
   
-  // Сохранить текущий текст и фокус
-  const previousText = commentInput ? commentInput.value : "";
-  const hasFocus = document.activeElement === commentInput;
+//   // Сохранить текущий текст и фокус
+//   const previousText = commentInput ? commentInput.value : "";
+//   const hasFocus = document.activeElement === commentInput;
   
-  // Перерисовать комментарии
-  renderComments(talkElement, comments);
+//   // Перерисовать комментарии
+//   renderComments(talkElement, comments);
   
-  // Восстановить текст и фокус
-  const newCommentInput = talkElement.querySelector(".new-comment");
-  if (newCommentInput) {
-    newCommentInput.value = previousText; // Восстановить текст
-    if (hasFocus) newCommentInput.focus(); // Восстановить фокус
+//   // Восстановить текст и фокус
+//   const newCommentInput = talkElement.querySelector(".new-comment");
+//   if (newCommentInput) {
+//     newCommentInput.value = previousText; // Восстановить текст
+//     if (hasFocus) newCommentInput.focus(); // Восстановить фокус
+//   }
+// }
+
+// // Функция для рендеринга комментариев
+// function renderComments(talkElement, comments) {
+//   const commentsContainer = talkElement.querySelector(".comments");
+//   commentsContainer.innerHTML = ""; // Очистить старые комментарии
+//   comments.forEach(comment => {
+//     const commentNode = document.createElement("div");
+//     commentNode.textContent = `${comment.author}: ${comment.message}`;
+//     commentsContainer.appendChild(commentNode);
+//   });
+// }
+
+// // Изменение функции renderTalk для использования updateCommentsUI
+// function renderTalk(talk, dispatch) {
+//   let talkElement = elt(
+//     "section", {className: "talk"},
+//     elt("h2", null, talk.title, " ", elt("button", {
+//       type: "button",
+//       onclick() {
+//         dispatch({type: "deleteTalk", talk: talk.title});
+//       }
+//     }, "Delete")),
+//     elt("div", null, "by ", elt("strong", null, talk.presenter)),
+//     elt("p", null, talk.summary),
+//     elt("div", {className: "comments"}, ...talk.comments.map(renderComment)),
+//     elt("form", {
+//       onsubmit(event) {
+//         event.preventDefault();
+//         let form = event.target;
+//         dispatch({type: "newComment", talk: talk.title, message: form.elements.comment.value});
+//         form.reset();
+//       }
+//     }, elt("input", {type: "text", name: "comment", className: "new-comment"}), " ",
+//        elt("button", {type: "submit"}, "Add comment"))
+//   );
+
+//   // Обновление комментариев при каждом вызове функции
+//   updateCommentsUI(talkElement, talk.comments);
+
+//   return talkElement;
+// }
+
+// function renderComment(comment) {
+//   return elt("p", {className: "comment"},
+//              elt("strong", null, comment.author),
+//              ": ", comment.message);
+// }
+
+
+/** Новая функция для работы с шаблонами */
+function instantiateTemplate(template, values) {
+  let copy = template.content.cloneNode(true);
+
+  // Поддержка template-repeat
+  for (let node of copy.querySelectorAll("[template-repeat]")) {
+    let parent = node.parentNode;
+    let array = values[node.getAttribute("template-repeat")];
+    if (!Array.isArray(array)) continue;
+
+    for (let item of array) {
+      let clone = node.cloneNode(true);
+      instantiateTemplate(clone, item);
+      parent.insertBefore(clone, node);
+    }
+    parent.removeChild(node); // Удаляем оригинальный узел
   }
+
+  // Поддержка template-if
+  for (let node of copy.querySelectorAll("[template-if]")) {
+    let condition = node.getAttribute("template-if");
+    if (!values[condition]) {
+      node.remove(); // Удаляем узел, если условие ложно
+    }
+  }
+
+  // Заполнение текста ({{ключ}})
+  for (let node of copy.querySelectorAll("[template-text]")) {
+    let key = node.getAttribute("template-text");
+    if (key in values) {
+      node.textContent = values[key];
+    }
+  }
+
+  return copy;
 }
 
-// Функция для рендеринга комментариев
-function renderComments(talkElement, comments) {
-  const commentsContainer = talkElement.querySelector(".comments");
-  commentsContainer.innerHTML = ""; // Очистить старые комментарии
-  comments.forEach(comment => {
-    const commentNode = document.createElement("div");
-    commentNode.textContent = `${comment.author}: ${comment.message}`;
-    commentsContainer.appendChild(commentNode);
+/** Изменённая функция drawTalk с использованием шаблонов */
+function drawTalk(talk, dispatch) {
+  let template = document.querySelector("#talk-template");
+  let element = instantiateTemplate(template, talk);
+
+  // Удаление темы
+  element.querySelector(".delete").addEventListener("click", () => {
+    dispatch({ type: "deleteTalk", talk: talk.title });
   });
+
+  // Добавление комментария
+  element.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let form = event.target;
+    dispatch({
+      type: "newComment",
+      talk: talk.title,
+      message: form.elements.comment.value,
+    });
+    form.reset();
+  });
+
+  return element;
 }
 
-// Изменение функции renderTalk для использования updateCommentsUI
-function renderTalk(talk, dispatch) {
-  let talkElement = elt(
-    "section", {className: "talk"},
-    elt("h2", null, talk.title, " ", elt("button", {
-      type: "button",
-      onclick() {
-        dispatch({type: "deleteTalk", talk: talk.title});
-      }
-    }, "Delete")),
-    elt("div", null, "by ", elt("strong", null, talk.presenter)),
-    elt("p", null, talk.summary),
-    elt("div", {className: "comments"}, ...talk.comments.map(renderComment)),
-    elt("form", {
-      onsubmit(event) {
-        event.preventDefault();
-        let form = event.target;
-        dispatch({type: "newComment", talk: talk.title, message: form.elements.comment.value});
-        form.reset();
-      }
-    }, elt("input", {type: "text", name: "comment", className: "new-comment"}), " ",
-       elt("button", {type: "submit"}, "Add comment"))
-  );
 
-  // Обновление комментариев при каждом вызове функции
-  updateCommentsUI(talkElement, talk.comments);
 
-  return talkElement;
-}
-
-function renderComment(comment) {
-  return elt("p", {className: "comment"},
-             elt("strong", null, comment.author),
-             ": ", comment.message);
-}
 
 function renderTalkForm(dispatch) {
   let title = elt("input", {type: "text"});
